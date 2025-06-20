4a7d054ca9c2a5a10d4a24386840a9902f380a12
Nema <nematoad00@gmail.com>
2025-06-20 10:56:42 -0400

    Basic Discord bot setup with ping command

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on(`messageCreate`, message => {
     if (message.author.bot) return;
 
     if(message.content === `!ping`) {
-        message.reply(`pong`);
+        message.reply(`pong!`);
     }
 });
 
